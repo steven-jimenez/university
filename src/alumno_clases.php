17529e5d5b98472af68ad5646892678db98d0f7c
Steven Jimenez <stevenjimenez00@hotmail.com>
2023-08-10 16:01:51 -0400

    se agrego el script js

--- src/alumno_clases.php
+++ src/alumno_clases.php
@@ -32,6 +32,7 @@ if ($resultado) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./output.css" rel="stylesheet">
+    <script src="./remove.js" defer></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Alumno</title>
 </head>
